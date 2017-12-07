0c3b7525b850a1da10bb9aaad6654d9fe6932e0c
Student <student@SETASs-Mac-mini-2.local>
2017-12-06 19:55:44 -0500

    Basic UI and labels added

---
+++ WunderList: To Do Tasks/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WunderList: To Do Tasks
+//
+//  Created by Student on 2017-12-06.
+//  Copyright © 2017 Centennial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var sliderLabel1: UILabel!
+    @IBOutlet weak var sliderLabel2: UILabel!
+    @IBOutlet weak var sliderLabel3: UILabel!
+    
+    @IBOutlet weak var sliderLabel4: UILabel!
+    @IBOutlet weak var sliderLabel5: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+  
+
+
+}
+
